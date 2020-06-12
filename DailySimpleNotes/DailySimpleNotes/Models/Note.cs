53948c981c0289112a81bd82e8c9d335f407bbf5
Piotr Khlystun <PiotrKhlystun@epam.com>
2020-06-12 22:21:10 +0300

    XamarinNotesUpdate

---
+++ DailySimpleNotes/DailySimpleNotes/Models/Note.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailySimpleNotes.Models
+{
+    [Table("Notes")]
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+
+        public string Title{ get; set; }
+        public string Text { get; set; }
+        public string CreationData { get; set; }
+
+        public string Color { get; set; }
+    }
+}
